73a47f93f2aa571c56da14b3f4c3278791258e3a
Pedro Rodrigues <prodrigues1990@gmail.com>
2019-07-11 00:42:50 +0100

    support nodes with same name, tests

--- tests/test_node.py
+++ tests/test_node.py
@@ -7,3 +7,13 @@ class TestNode(TestCase):
         with self.assertRaises(ValueError):
             Node('', '', 0)
             Node('', 0, '',)
+
+    def test_nodesWithSwappedLatLng_dontCollide(self):
+        node_a1 = Node(1, 2, 'a')
+        node_a2 = Node(2, 1, 'a')
+        node_b = Node(2, 1, 'b')
+        self.assertNotEqual(node_a1.__hash__(), node_a2.__hash__())
+        self.assertNotEqual(node_a1, node_a2)
+
+        self.assertNotEqual(node_a2.__hash__(), node_b.__hash__())
+        self.assertNotEqual(node_a2, node_b)
